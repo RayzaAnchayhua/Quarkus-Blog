51e51558f29dc71184af8dfe9562e6535b1c318f
Rayza Luana <93015921+RayzaAnchayhua@users.noreply.github.com>
2024-10-27 20:54:58 -0300

    feat: dark mode and styles

--- src/main/resources/web/app/main.js
+++ src/main/resources/web/app/main.js
@@ -1,1 +1,15 @@
-import './roq.scss'
+import "./roq.scss";
+
+function toggleTheme() {
+  const body = document.body;
+
+  if (body.classList.contains("theme--light")) {
+    body.classList.replace("theme--light", "theme--dark");
+    document.getElementsByClassName("change-mode")[0].innerHTML =
+      "Switch to Light Mode";
+  } else {
+    body.classList.replace("theme--dark", "theme--light");
+    document.getElementsByClassName("change-mode")[0].innerHTML =
+      "Switch to Dark Mode";
+  }
+}
