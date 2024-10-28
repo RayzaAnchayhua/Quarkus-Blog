307a28fbc9fdbcf53ce3a62f7e1305c9ad0a263b
RayzaAnchayhua <rayzaanchayhua@gmail.com>
2024-10-28 15:19:43 -0300

    fix: Responsive sidebar

--- src/main/resources/web/app/main.js
+++ src/main/resources/web/app/main.js
@@ -1,15 +1,4 @@
 import "./roq.scss";
-
-function toggleTheme() {
-  const body = document.body;
-
-  if (body.classList.contains("theme--light")) {
-    body.classList.replace("theme--light", "theme--dark");
-    document.getElementsByClassName("change-mode")[0].innerHTML =
-      "Switch to Light Mode";
-  } else {
-    body.classList.replace("theme--dark", "theme--light");
-    document.getElementsByClassName("change-mode")[0].innerHTML =
-      "Switch to Dark Mode";
-  }
-}
+import "./dark.scss";
+import "./light.scss";
+import "./media.scss";
